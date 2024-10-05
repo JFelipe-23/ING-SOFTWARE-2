58253e48c96592fd32f1a0ed24653d3bb6e494ff
David Garcia <juandavidgarciaburbano@gmail.com>
2024-10-04 20:04:28 -0500

    Desarrollo del models y tests (falta un punto del tests)  Changes to be committed:     modified:   Gestion_inventarios/Login/tests.py  Changes not staged for commit: 	modified:   Gestion_inventarios/Login/models.py 	modified:   Gestion_inventarios/Login/tests.py

--- Gestion_inventarios/Login/tests.py
+++ Gestion_inventarios/Login/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class Login_tests(TestCase):
+    def setUp(self):
+        self.user = User("Juan18", "contraseña18")
+
+    def test_autenticacion_exitosa(self):
+        resultado = self.user.autenticar("Juan18", "contraseña18")
+        self.assertEqual(resultado, "Autenticación exitosa")
+#--------------------------------------------------------------------------------------
+#    def test_autenticacion_fallida(self):}
+
+#---------------------------------------------------------------------------------------
+    def test_cambiar_contraseña(self):
+
+        self.user.cambiar_contraseña("David18")
+        resultado = self.user.autenticar("Juan18", "David18")
+        self.assertEqual(resultado, "Autenticación exitosa")
+#---------------------------------------------------------------------------------------
+    def test_cambiar_usuario(self):
+        # Cambiar usuario y verificar autenticación
+        self.user.cambiar_usuario("NuevoPato")
+        resultado = self.user.autenticar("NuevoPato", "contraseña123")
+        self.assertEqual(resultado, "Autenticación exitosa")
+
+
