1022f07bc75384339659b653688a0af4b5177570
JuanDavid601 <lhdj201987@gmail.com>
2024-10-10 14:34:04 -0500

    Corregido modelo en base a BD

--- Gestion_inventarios/Provedores/models.py
+++ Gestion_inventarios/Provedores/models.py
@@ -1,23 +1,25 @@
 from django.db import models
-from Gestion_inventarios import Caja
+from Gestion_inventarios import Product
 
-class Proovedor:
-    def __init__(self, Nombre, direccion, telefono, ID,):
-        self.Nombre=Nombre
-        self.Direccion=direccion
-        self.telefono=telefono
-        self.ID=ID
-        self.Productos=[]
+class Proovedor (models.Model):
+    Nombre=models.CharField(max_length=150)
+    Direccion=models.CharField(max_length=200)
+    Telefono=models.CharField(max_length=50)
+    ID=models.IntegerField(max_length=10)
+    Productos= models.ForeignKey(Product)
 
     def __str__(self):
         return f"Proveedor: {self.nombre}\n
         Dirección: {self.direccion}\n
         Teléfono: {self.telefono}\n
         Productos: {self.productos}"
+    
+    def __str__(self):
+        return f"Productos: {self.Productos}"
 
-    def agregar_producto(self, producto):
-        self.productos.append(producto)
+    def agregar_producto(self, Productos):
+        self.productos.append(Productos)
 
-    def quitar_producto(self,producto):
-        if producto in self.Productos:
-            self.Productos.remove(producto)
+    def quitar_producto(self,Producto):
+        if Producto in self.Productos:
+            self.Productos.remove(Producto)
