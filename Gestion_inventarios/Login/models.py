811576b50cc20bcb79c49ba8829aaeecdc4e0a8f
David Garcia <juandavidgarciaburbano@gmail.com>
2024-10-11 15:47:36 -0500

    Modelo corregido(Deacuerdo al ultimo daily)  Changes to be committed:

--- Gestion_inventarios/Login/models.py
+++ Gestion_inventarios/Login/models.py
@@ -7,16 +7,37 @@ class User:
         self.usuario = usuario
         self.contraseña = Contraseña
 
-#--------------------------------------------------------------------------
+
+class User(models.Model):
+    usuario = models.CharField(max_length=20)
+    contraseña = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Usuario: {self.usuario}"
+
+#------------------------------------------------------------------------
     def autenticar(self, usuario, contraseña):
         if self.usuario == usuario and self.contraseña == contraseña:
             return "Autenticación exitosa"
         else:
             return "Usuario o contraseña incorrectos"
-#----------------------------------------------------------------------------  
+#---------------------------------------------------------------------------
     def cambiar_contraseña(self, nueva_contraseña):
         self.contraseña = nueva_contraseña
-#----------------------------------------------------------------------------
+        self.save()
+#-----------------------------------------------------------------------------------
     def cambiar_usuario(self, nuevo_usuario):
         """Actualiza el nombre de usuario."""
         self.usuario = nuevo_usuario
+        self.save()
+
+
+
+
+
+
+
+
+
+
+
