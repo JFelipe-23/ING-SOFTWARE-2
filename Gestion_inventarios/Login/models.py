3ed0636ab02205a96e9eb6955cc3c2072e9b11a2
David Garcia <juandavidgarciaburbano@gmail.com>
2024-10-16 00:37:55 -0500

    Login clisto presentacion (Falta las views.py) 	modified:   Gestion_inventarios/Login/models.py 	modified:   Gestion_inventarios/Login/tests.py

--- Gestion_inventarios/Login/models.py
+++ Gestion_inventarios/Login/models.py
@@ -2,39 +2,30 @@ from django.db import models
 
 # Create your models here.
 
-class User:
-    def __init__(self,  usuario, Contraseña):
-        self.usuario = usuario
-        self.contraseña = Contraseña
-
+from django.db import models
 
 class User(models.Model):
     usuario = models.CharField(max_length=20)
     contraseña = models.CharField(max_length=10)
 
+# Metodos
+#-----------------------------------------------------------------------------------
     def __str__(self):
         return f"Usuario: {self.usuario}"
-
-#------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
     def autenticar(self, usuario, contraseña):
         if self.usuario == usuario and self.contraseña == contraseña:
             return "Autenticación exitosa"
         else:
             return "Usuario o contraseña incorrectos"
-#---------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
     def cambiar_contraseña(self, nueva_contraseña):
         self.contraseña = nueva_contraseña
         self.save()
 #-----------------------------------------------------------------------------------
     def cambiar_usuario(self, nuevo_usuario):
-        """Actualiza el nombre de usuario."""
         self.usuario = nuevo_usuario
         self.save()
-
-
-
-
-
 
 
 
--- Gestion_inventarios/Login/tests.py
+++ Gestion_inventarios/Login/tests.py
@@ -1,28 +1,36 @@
 from django.test import TestCase
-from .models import User
+from models import User
 # Create your tests here.
 
-class Login_tests(TestCase):
+
+
+class UserModelTest(TestCase):
+
     def setUp(self):
-        self.user = User("Juan18", "contraseña18")
+
+        self.user = User.objects.create(usuario="David17", contraseña="123456789")
 
     def test_autenticacion_exitosa(self):
-        resultado = self.user.autenticar("Juan18", "contraseña18")
+        resultado = self.user.autenticar("David17", "123456789")
         self.assertEqual(resultado, "Autenticación exitosa")
-#--------------------------------------------------------------------------------------
-#    def test_autenticacion_fallida(self):}
-
-#---------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+    def test_autenticacion_fallida(self):
+        resultado = self.user.autenticar("David17", "13456789")
+        self.assertEqual(resultado, "Usuario o contraseña incorrectos")
+#--------------------------------------------------------------------------------------------------
     def test_cambiar_contraseña(self):
+        self.user.cambiar_contraseña("987654321")
+        self.user.refresh_from_db() 
+        self.assertEqual(self.user.contraseña, "987654321")
 
-        self.user.cambiar_contraseña("David18")
-        resultado = self.user.autenticar("Juan18", "David18")
+        resultado = self.user.autenticar("Pato", "987654321")
         self.assertEqual(resultado, "Autenticación exitosa")
-#---------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
     def test_cambiar_usuario(self):
-        # Cambiar usuario y verificar autenticación
         self.user.cambiar_usuario("David18")
-        resultado = self.user.autenticar("David18", "contraseña18")
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.usuario, "David18")
+      
+        resultado = self.user.autenticar("David18", "987654321")
         self.assertEqual(resultado, "Autenticación exitosa")
-
 
